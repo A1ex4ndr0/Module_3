4d502768f0337915d5bf34e4595f704baa269fd4
A1ex4ndr0 <a1ex.n.brb@gmail.com>
2024-10-31 23:24:35 +0300

    Пространство имён

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    cort = (len(string), string.upper(), string.lower())
+    return cort
+def is_contains(string, list_to_search):
+    count_calls()
+    k = False
+    for i in list_to_search:
+        if string.upper() == i.upper():
+            k = True
+            break
+        else:
+            k = False
+    return k
+
+print(string_info('Alexander'))
+print(string_info('Nikolaevich'))
+print(string_info('Barabolya'))
+print(is_contains('Alex', ['PAlec', 'aLeX', 'alexia']))
+print(is_contains('pin', ['pincode', 'KraPiva', 'PiPin']))
+print(is_contains('soVA', ['SovA', 'roSOVAnie', 'Sovok']))
+print(calls)
