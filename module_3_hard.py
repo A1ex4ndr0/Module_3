ea49d11573103877ce1ec01b2fdc98a3f7988983
A1ex4ndr0 <a1ex.n.brb@gmail.com>
2024-11-09 23:42:25 +0300

    Дополнительное практическое задание по модулю 3

---
+++ module_3_hard.py
@@ -0,0 +1,23 @@
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+], 5, {'peoples': [5, '7'], 12: {2: 'gold'}}
+
+def calculate_structure_sum(structure_):
+    result_ = 0
+    for i in structure_:
+        if isinstance(i, str):
+            result_ += len(i)
+        elif isinstance(i, int):
+            result_ += i
+        elif isinstance(i, dict):
+            for j in i.items():
+                result_ += calculate_structure_sum(j)
+        else:
+           result_ += calculate_structure_sum(i)
+    return result_
+
+print(calculate_structure_sum(data_structure))
